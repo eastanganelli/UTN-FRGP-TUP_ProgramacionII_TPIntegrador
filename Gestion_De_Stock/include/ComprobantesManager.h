5300af2e4d67988a4d63ad633df4515234a03a34
eastanganelli <ezequielstanganelli+git@gmail.com>
2025-11-09 23:13:59 -0300

    Add ComprobantesManager class and update project files for inclusion

---
+++ Gestion_De_Stock/include/ComprobantesManager.h
@@ -0,0 +1,14 @@
+#ifndef COMPROBANTESMANAGER_H
+#define COMPROBANTESMANAGER_H
+
+
+class Comprobantesmanager
+{
+    public:
+        Comprobantesmanager();
+        ~Comprobantesmanager();
+
+    private:
+};
+
+#endif // COMPROBANTESMANAGER_H
---
+++ Gestion_De_Stock/src/ComprobantesManager.cpp
@@ -0,0 +1,11 @@
+#include "Comprobantesmanager.h"
+
+Comprobantesmanager::Comprobantesmanager()
+{
+    //ctor
+}
+
+Comprobantesmanager::~Comprobantesmanager()
+{
+    //dtor
+}
